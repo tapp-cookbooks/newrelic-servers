35f3821132b3c11d6a7ef79ac8547ca6543ad8ce
Javier Perez-Griffo <javier@besol.es>
2013-07-05 12:24:20 +0200

    Updated requirements for newrelic cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,17 +1,15 @@
-maintainer       "Heavy Water Software Inc."
-maintainer_email "ops@heavywater.ca"
+maintainer "Besol Soluciones S.L."
+maintainer_email "contacto@besol.es"
 license          "Apache 2.0"
 description      "Installs/Configures newrelic"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.11"
+version          "0.2.12"
 
-supports "ubuntu"
-supports "debian"
-supports "centos"
-supports "redhat"
-supports "fedora"
-supports "windows"
+%w(windows apt).each do |recipe_suggests|
+  suggests recipe_suggests
+end
 
-depends "apt"
-depends "windows"
+%w{ ubuntu debian centos redhat scientific windows}.each do |os|
+  supports os
+end
 
